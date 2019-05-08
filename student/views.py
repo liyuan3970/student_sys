8bcbd1497ee9f6d1af4df81a52eafa5c43de940d
liyuan3970 <857956255@qq.com>
2019-05-08 21:52:26 +0800

    liyuan

--- student/views.py
+++ student/views.py
@@ -5,21 +5,43 @@ from django.http import HttpResponseRedirect
 
 from .forms import StudentForm
 from .models import Student
+from django.views import View
 
 
 # Create your views here.
-def index(requset):
-
-    students = Student.get_all()
-    if requset.method == 'POST':
-        form = StudentForm(requset.POST)
+# def index(requset):
+#
+#     students = Student.get_all()
+#     if requset.method == 'POST':
+#         form = StudentForm(requset.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('index'))
+#     else:
+#         form = StudentForm()
+#     context = {
+#         'students': students,
+#         'form': form,
+#     }
+#     return render(requset, 'index.html', context=context)
+class IndexView(View):
+    template_name = 'index.html'
+    def get_context(self):
+        students=Student.get_all()
+        context = {
+            'students':students,
+        }
+        return context
+    def get(self,request):
+        context = self.get_context()
+        form =StudentForm()
+        context.update({'form':form})
+        return render(request,self.template_name,context=context)
+    def post(self,request):
+        form =StudentForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('index'))
-    else:
-        form = StudentForm()
-    context = {
-        'students': students,
-        'form': form,
-    }
-    return render(requset, 'index.html', context=context)
+        context = self.get_context()
+        context.update({'form':form})
+        return render(request,self.template_name,context=context)
